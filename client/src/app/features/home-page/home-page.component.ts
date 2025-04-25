5586cb43b4e2caaace2ddf99f9034c5136f6f857
Izet Delibasic <izet.delibasic@htecgroup.com>
2025-04-25 15:04:32 +0200

    Adding jwtInterceptor, creating member-card component and members service

--- client/src/app/features/home-page/home-page.component.ts
+++ client/src/app/features/home-page/home-page.component.ts
@@ -1,7 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RegisterFormComponent } from '../../components/register-form/register-form.component';
-import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home-page',
@@ -9,14 +7,8 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
   registerMode = false;
-  users: any;
-  http = inject(HttpClient);
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle() {
     console.log(this.registerMode);
@@ -25,13 +17,5 @@ export class HomePageComponent implements OnInit {
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = !event;
-  }
-
-  getUsers() {
-    this.http.get(environment.apiBaseUrl + 'user').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
   }
 }
