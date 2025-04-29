7fa0ca90abb4c879a94bdca79d71940c61827ea2
Izet Delibasic <izet.delibasic@htecgroup.com>
2025-04-29 15:59:24 +0200

    Importing pagination for members

--- client/src/app/features/members/members-list/members-list.component.ts
+++ client/src/app/features/members/members-list/members-list.component.ts
@@ -1,23 +1,31 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../../services/members.service';
-import { Member } from '../../../models/member';
 import { MemberCardComponent } from '../../../components/members/member-card/member-card.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-members-list',
-  imports: [MemberCardComponent],
+  imports: [MemberCardComponent, PaginationModule],
   templateUrl: './members-list.component.html',
   styleUrl: './members-list.component.css',
 })
 export class MembersListComponent implements OnInit {
   membersService = inject(MembersService);
-  members: Member[] = [];
+  pageNumber = 1;
+  pageSize = 5;
 
   ngOnInit(): void {
-    if (this.membersService.members().length === 0) this.loadMembers();
+    if (!this.membersService.paginatedResult()) this.loadMembers();
   }
 
   loadMembers() {
-    this.membersService.getMembers();
+    this.membersService.getMembers(this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,71 +1,92 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../environments/environment';
 import { Member } from '../models/member';
 import { of, tap } from 'rxjs';
 import { Photo } from '../models/photo';
+import { PaginatedResult } from '../models/pagination';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MembersService {
   private http = inject(HttpClient);
-  members = signal<Member[]>([]);
+  // members = signal<Member[]>([]);
+  paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
 
-  getMembers() {
-    return this.http.get<Member[]>(environment.apiBaseUrl + 'user').subscribe({
-      next: (members) => this.members.set(members),
-    });
+  getMembers(pageNumber?: number, pageSize?: number) {
+    let params = new HttpParams();
+
+    if (pageNumber && pageSize) {
+      params = params.append('pageNumber', pageNumber);
+      params = params.append('pageSize', pageSize);
+    }
+
+    return this.http
+      .get<Member[]>(environment.apiBaseUrl + 'user', {
+        observe: 'response',
+        params,
+      })
+      .subscribe({
+        next: (response) => {
+          this.paginatedResult.set({
+            items: response.body as Member[],
+            pagination: JSON.parse(response.headers.get('Pagination')!),
+          });
+        },
+      });
   }
 
   getMember(username: string) {
-    const member = this.members().find((x) => x.userName === username);
-    if (member !== undefined) return of(member);
+    // const member = this.members().find((x) => x.userName === username);
+    // if (member !== undefined) return of(member);
 
     return this.http.get<Member>(environment.apiBaseUrl + `user/${username}`);
   }
 
   updateMember(member: Member) {
-    return this.http.put(environment.apiBaseUrl + 'user', member).pipe(
-      tap(() => {
-        this.members.update((members) =>
-          members.map((m) => (m.userName === member.userName ? member : m))
-        );
-      })
-    );
+    return this.http
+      .put(environment.apiBaseUrl + 'user', member)
+      .pipe
+      // tap(() => {
+      //   this.members.update((members) =>
+      //     members.map((m) => (m.userName === member.userName ? member : m))
+      //   );
+      // })
+      ();
   }
 
   setMainPhoto(photo: Photo) {
     return this.http
       .put(environment.apiBaseUrl + `user/set-main-photo/${photo.id}`, {})
-      .pipe(
-        tap(() => {
-          this.members.update((members) =>
-            members.map((m) => {
-              if (m.photos.includes(photo)) {
-                m.photoUrl = photo.url;
-              }
-              return m;
-            })
-          );
-        })
-      );
+      .pipe
+      // tap(() => {
+      //   this.members.update((members) =>
+      //     members.map((m) => {
+      //       if (m.photos.includes(photo)) {
+      //         m.photoUrl = photo.url;
+      //       }
+      //       return m;
+      //     })
+      //   );
+      // })
+      ();
   }
 
   deletePhoto(photo: Photo) {
     return this.http
       .delete(environment.apiBaseUrl + `user/delete-photo/${photo.id}`)
-      .pipe(
-        tap(() => {
-          this.members.update((members) =>
-            members.map((m) => {
-              if (m.photos.includes(photo)) {
-                m.photos = m.photos.filter((x) => x.id !== photo.id);
-              }
-              return m;
-            })
-          );
-        })
-      );
+      .pipe
+      // tap(() => {
+      //   this.members.update((members) =>
+      //     members.map((m) => {
+      //       if (m.photos.includes(photo)) {
+      //         m.photos = m.photos.filter((x) => x.id !== photo.id);
+      //       }
+      //       return m;
+      //     })
+      //   );
+      // })
+      ();
   }
 }
