b90e698112d10d507e5a36f540fd7c70220bc9ab
Izet Delibasic <izet.delibasic@htecgroup.com>
2025-06-21 17:57:31 +0200

    Creating skeleton loading for messenger part

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -1,10 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { finalize } from 'rxjs';
+import { delay, finalize } from 'rxjs';
 import { LoadingService } from '../services/loading.service';
+import { MESSAGES_API } from '../constants/servicesConstants/messagesServiceConstant';
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoadingService);
+
+  if (req.url.includes(MESSAGES_API.BASE)) {
+    return next(req).pipe(delay(1000));
+  }
 
   loadingService.setLoading(true);
 
